e5d3991ace1a8c9a3fb5f16a822649b73c994659
Erik Culberson <eculberson12@gmail.com>
2015-04-03 17:57:44 -0400

    Moved files

--- CodeEval/EasyChallenges/FizzBuzz/FizzBuzz.hs
+++
@@ -1,7 +0,0 @@
-import System.Environment (getArgs)
-
-  main = do
-  [inpFile] <- getArgs
-  input <- readFile inpFile
-
-
--- CodeEval/EasyChallenges/OddNumbers/OddNumbers.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-  int i = 1;
-  for(i=1; i < 100; i+=2) {
-    printf("%d\n", i);
-  }
-  return 0;
-}
